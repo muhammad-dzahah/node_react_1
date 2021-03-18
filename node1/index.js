fc0e00e74ee02ca9b164ab819145b475948713ce
muhammad-dzahah <60204748+muhammad-dzahah@users.noreply.github.com>
2021-03-18 17:26:07 +0700

    Update index.js

--- node1/index.js
+++ node1/index.js
@@ -1,15 +1,31 @@
+//inisialisasi library
 const express = require("express")
 const app = express()
 
 //import fungsi authorization auth
 const auth = require("./auth")
 
+//import route user
 const user = require("./route/user")
 app.use("/", user)
 
+//import route pegawai
 const pegawai = require("./route/pegawai")
-app.use("/pegawai", pegawai)
+app.use("/pegawai",auth, pegawai)
 
+//import route pelanggaran
+const pelanggaran = require("./route/pelanggaran")
+app.use("/pelanggaran", auth, pelanggaran)
+
+//import route siswa
+const siswa = require("./route/siswa")
+app.use("/siswa", auth, siswa)
+
+//import route jurusan
+const jurusan = require("./route/jurusan")
+app.use("/jurusan", auth, jurusan)
+
+//membuat web server dengan port 2000
 app.listen(2000, () => {
-    console.log("Server run on port 2000");
+    console.log("server run on port 2000")
 })
